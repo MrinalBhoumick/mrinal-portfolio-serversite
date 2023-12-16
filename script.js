b7344a735cd5d2ca23b6dbf33513e5c06e4ac5f9
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2023-12-16 14:07:32 +0530

    Updated C.v

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Coder", "Designer", "Freelancer","Gamer"],
+        strings: ["Cloud Engineer", "Developer", "Coder","Photographer","Gamer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Coder", "Designer", "Freelancer","Gamer"],
+        strings: ["Cloud Engineer", "Developer", "Coder","Photographer","Gamer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
